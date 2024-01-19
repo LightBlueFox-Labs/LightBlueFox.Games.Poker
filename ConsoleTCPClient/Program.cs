a24caaa3b77d9098755079e5f23478c98fe52cd0
TINHOFER Jakob <jak.tinhofer@billrothgymnasium.at>
2024-01-19 02:35:44 +0100

    Added ip, port as cli args

--- ConsoleTCPClient/Program.cs
+++ ConsoleTCPClient/Program.cs
@@ -4,13 +4,23 @@ using LightBlueFox.Connect.Structure;
 using LightBlueFox.Connect.Structure.Validators;
 using LightBlueFox.Games.Poker.PlayerHandles;
 using LightBlueFox.Games.Poker.PlayerHandles.Remote;
+using System.Net;
 
 // See https://aka.ms/new-console-template for more information
+string addr = "localhost"; int port = 12332;
 
+if(args.Length == 2)
+{
+    addr = (args[0] == "localhost" ? "localhost" : IPAddress.Parse(args[0]).ToString());
+    port = int.Parse(args[1]);
+}
+ 
+
+Console.WriteLine($"IP: {addr}, Port: {port}");
 Console.Write("Enter your name: ");
 var name = Console.ReadLine();
 
-ProtocolConnection c = ProtocolConnection.CreateWithValidation(PokerProtocol.BuildProtocol(), new TcpConnection("localhost", 12332), ConnectionNegotiationPosition.Challenger);
+ProtocolConnection c = ProtocolConnection.CreateWithValidation(PokerProtocol.BuildProtocol(), new TcpConnection(addr.ToString(), port), ConnectionNegotiationPosition.Challenger);
 
 ConsolePlayer p = new ConsolePlayer(name);
 Task.Delay(1000).Wait();    
--- ConsoleTCPServer/Program.cs
+++ ConsoleTCPServer/Program.cs
@@ -6,15 +6,23 @@ using LightBlueFox.Connect.Structure;
 using LightBlueFox.Games.Poker;
 using LightBlueFox.Games.Poker.PlayerHandles;
 using LightBlueFox.Games.Poker.PlayerHandles.Remote;
+using System.Net;
 
-Console.WriteLine("Creating new game.");
+IPAddress listenerAddr = args[0] == "ALL" ? IPAddress.Any : IPAddress.Parse(args[0]);
+int port = int.Parse(args[1]);
+
+
+Console.WriteLine("[SERVER] Creating new game.");
 Game game = new("test");
+Console.WriteLine("[SERVER] Created game..");
 
-Console.WriteLine("Created game..");
-Console.WriteLine("Starting server on port 12332...");
+Console.WriteLine($"[SERVER] Starting server {listenerAddr}:{port}...");
 
 ProtocolServer s = new(PokerProtocol.BuildProtocol(), new[] { new TcpSource(12332) });
 s.OnConnectionValidated += newPlayer;
+
+Console.WriteLine("[SERVER] Now listening.");
+
 
 Console.ReadKey(true);
 
@@ -23,7 +31,7 @@ Console.ReadKey(true);
 
 while (true) {
     if (game.Players.Count > 1) {
-        Console.WriteLine("Enough players connected. Starting (next) round.");
+        Console.WriteLine("[SERVER] Enough players connected. Starting (next) round.");
         game.startRound();
     }
     Task.Delay(100).Wait(); 
@@ -32,6 +40,6 @@ while (true) {
 
 void newPlayer(ProtocolConnection c, ProtocolServer sender)
 {
-    Console.WriteLine("New Player Connected.");
+    Console.WriteLine("[SERVER] New Player Connected.");
     game.AddPlayer(RemotePlayer.CreatePlayer(c));
 }
