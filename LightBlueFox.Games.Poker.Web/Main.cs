ce209bea7577c38f9d78684216140c2eec18d0bc
Jakob Tinhofer <>
2025-04-04 21:46:34 +0200

    changed namespaces

--- LightBlueFox.Games.Poker.Web/Main.cs
+++ LightBlueFox.Games.Poker.Web/Main.cs
@@ -8,11 +8,11 @@ namespace LightBlueFox.Games.Poker.Web
 		public static void Main(string[] args)
 		{
 			var builder = WebApplication.CreateBuilder(args);
+			
 			builder.Services.AddRazorPages();
 			builder.Services.AddServerSideBlazor();
 
 			var app = builder.Build();
-
 			if (!app.Environment.IsDevelopment())
 			{
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
